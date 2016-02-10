347bf5aef0ff8e67ea849198fb011d1fded931b5
Suhail <suhailgupta03@gmail.com>
2016-02-10 13:15:20 +0530

    reverted carousel

--- structure/index.php
+++ structure/index.php
@@ -1,47 +1,42 @@
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-      </ol>
+   <!-- Header Carousel -->
+    <header id="myCarousel" class="carousel slide">
+       <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+       </ol>
 
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="./images/1900x1080px.jpg" alt="...">
-          <div class="carousel-caption">
-            ...
+        <!-- Wrapper for slides -->
+-        <div class="carousel-inner">           
+            <div class="item active">
+                <div class="fill" style="background-image:url('./images/migrate-to-canada1.jpg');"></div>
+<!--                 <div class="carousel-caption"> -->
+<!--                     <h2>Caption 1</h2> -->
+<!--                 </div> -->
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('./images/visas-banner-USA.jpg');"></div>
+<!--                 <div class="carousel-caption"> -->
+<!--                     <h2>Caption 2</h2> -->
+<!--                 </div> -->
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('./images/sl3.jpg');"></div>
+<!--                 <div class="carousel-caption"> -->
+<!--                     <h2>Caption 3</h2> -->
+<!--                 </div> -->
+            </div>
           </div>
-        </div>
 
-        <div class="item">
-          <img src="./images/migrate-to-canada1.jpg" alt="...">
-          <div class="carousel-caption">
-            ...
-          </div>
-        </div>
-
-         <div class="item">
-          <img src="./images/sl3.jpg" alt="...">
-          <div class="carousel-caption">
-            ...
-          </div>
-        </div>
-
-        ...
-      </div>
-
-      <!-- Controls -->
-      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
+            <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+    </header>
 
     <!-- Page Content -->
     <div class="container-fluid">
