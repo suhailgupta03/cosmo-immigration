1627bad4867c7efbf6eedde5898deb467a1db9f4
suhailgupta03 <suhailgupta03@gmail.com>
2016-02-13 02:17:42 +0530

    enhanced footer for various screen resolutions

--- footer.php
+++ footer.php
@@ -10,16 +10,15 @@
 	        <div id='share' class='text-center'></div>
 	        <div class="row">
 			    <div class="col-md-12">
-			      <div class="col-md-3 col-sm-3 col-xs-3">
+			      <div class="col-md-3 col-md-offset-0 col-sm-3 col-sm-offset-3 col-xs-5 col-xs-offset-1">
 			        <ul class="nav nav-pills nav-stacked">
 			          <li><a href="./about-us.php">About us</a></li>
 			          <li><a href="#">Photo Gallery</a></li>
 			          <li><a href="#">Video Gallery</a></li>
 			          <li><a href="#">Jobs @ CIMM</a></li>
-			          <li><a href="#">Sitemap</a></li>
 			        </ul>
 			      </div>
-			      <div class="col-md-3 col-sm-3 col-xs-3">
+			      <div class="col-md-3 col-sm-3 col-xs-6">
 			        <ul class="nav nav-pills nav-stacked">
 			          <li><a href="http://www.topuniversities.com/where-to-study/north-america/canada/top-universities-canada-201516">Study In Canda</a></li>
 			          <li><a href="http://www.topuniversities.com/where-to-study/north-america/united-states/guide">Study In USA</a></li>
@@ -28,7 +27,7 @@
 			          <li><a href="http://www.topuniversities.com/where-to-study/oceania/new-zealand/guide">Study In New Zealand</a></li>
 			        </ul>
 			      </div>
-			      <div class="col-md-3 col-sm-3 col-xs-3">
+			      <div class="col-md-3 col-md-offset-0 col-sm-3 col-sm-offset-3 col-xs-5 col-xs-offset-1">
 			        <ul class="nav nav-pills nav-stacked">
 			          <li><a href="#">Write a Query</a></li>
 			          <li><a href="#">Register for Newsletters</a></li>   
@@ -37,7 +36,7 @@
 			          <li><a href="./faq.php">FAQ's</a></li>                             
 			        </ul>
 			      </div>
-			      <div class="col-md-3 col-sm-3 col-xs-3">
+			      <div class="col-md-3 col-sm-3 col-xs-6">
 			        <ul class="nav nav-pills nav-stacked">
 			          <li><a href="#">University Selection</a></li>
 			          <li><a href="#">Career Counselling</a></li>
@@ -49,12 +48,13 @@
 	  		</div>
 	 		 <hr>
 		    <div class="row">
-		        <div class="col-lg-12">
-		            <ul class="nav nav-justified">
-		                <li><a href="/">© 2016 Cosmo Immigration</a></li>
-		                <li><a href="#">Terms of Service</a></li>
-		                <li><a href="#">Privacy</a></li>
-		            </ul>
+		        <div class='text-left col-md-4 col-sm-4 hidden-xs bottom-content'>© 2016 Cosmo Immigration</div>
+		        <div class='text-left col-xs-3 visible-xs bottom-content'>© 2016</div>
+		        <div class='text-right col-md-8 col-sm-8 col-xs-9 bottom-content'>
+		        	<a href="#" class='footer-links'>Terms of Service</a>
+		        	<a href="#" class='footer-links'>Privacy Policy</a>
+		        	<a href="#" class='hidden-xs footer-links'>Disclaimer</a>
+		        	<a href="#" class='hidden-xs footer-links'>Sitemap</a>
 		        </div>
 		    </div>
         </footer>
